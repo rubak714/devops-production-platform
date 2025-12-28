4aa93591dac253bc35d3a9a2e5984fe4b4743378
rubak714 <rubaiyakabir889@gmail.com>
2025-12-28 21:37:36 +0100

    Enhance /work endpoint to increase CPU usage for HPA demo

--- app/main.py
+++ app/main.py
@@ -9,9 +9,17 @@ def home():
     return "DevOps Production App is running"
 
 @app.route("/work")
+# def work():
+#     time.sleep(random.uniform(0.1, 0.5))
+#     return "Work done"
+
 def work():
-    time.sleep(random.uniform(0.1, 0.5))
-    return "Work done"
+    # heavier CPU task
+    total = 0
+    for i in range(10000000):
+        total += i % 5
+    time.sleep(random.uniform(0.1, 0.3))
+    return f"Work done {total}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
