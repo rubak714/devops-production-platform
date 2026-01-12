e921d4edece730a3019e929136c73aca6dbe06ee
rubak714 <rubaiyakabir889@gmail.com>
2026-01-13 00:17:39 +0100

    Define input variables for GKE infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  default = "plated-shelter-483316-q9"
+}
+
+variable "region" {
+  default = "europe-west3"
+}
+
+variable "cluster_name" {
+  default = "devops-gke-cluster"
+}
